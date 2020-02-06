3f85059ac78718d379704f2251ae7f67e67a295e
injoinD0913 <55337212+injoinD0913@users.noreply.github.com>
2020-02-06 20:10:04 +0800

    Update demo-udp.py

--- demo-udp.py
+++ demo-udp.py
@@ -4,8 +4,17 @@ def main():
 	# 创建一个udp套接字
 	udp_socket = socket.socket(socket.AF_INET,sockey.SOCK_DGRAM)
 	
-	# 使用套接字收发数据
-	udp_socket.sendto(b"hahaha",("192.168.56.1 ",8080))
+	while True:
+		
+		# 从键盘获取数据
+		send_data = input("请输入要发送的数据：")
+		
+		# 如果输入的数据是exit，那么就退出程序
+		if send_data = "exit":
+			break
+			
+		# 使用套接字收发数据
+		udp_socket.sendto(send_data.encode("utf-8"),("192.168.56.1 ",8080))
 	
 	#关闭套接字
 	udp_socket.close()
